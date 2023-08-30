7fa3640dfab1a37dbc26b9ac3d7b7dc33b6c2a18
kimotu4632uz <kimotu.phone@gmail.com>
2023-08-30 10:20:34 +0900

    fix template.cpp

--- .devcontainer/atcoder-cli-nodejs/cpptemp/template.cpp
+++ .devcontainer/atcoder-cli-nodejs/cpptemp/template.cpp
@@ -4,8 +4,8 @@ using ll = long long;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-  return 0;
+    return 0;
 }
