28b38a6fb9859068fa1abcd84d9a9ce780f6153f
kimotu4632uz <kimotu.phone@gmail.com>
2023-10-21 10:49:14 +0000

    update template

--- .devcontainer/atcoder-cli-nodejs/cpptemp/template.cpp
+++ .devcontainer/atcoder-cli-nodejs/cpptemp/template.cpp
@@ -3,7 +3,6 @@ using namespace std;
 using ll = long long;
 constexpr auto seq(int begin, int end) { return ranges::iota_view(begin, end); }
 constexpr auto seq(int end) { return ranges::iota_view(0, end); }
-#define all(v) v.begin(), v.end()
 
 int main() {
     ios_base::sync_with_stdio(false);
