cfb64b0396802ec5d85a84f549a91b343d8b4ca9
kimotu4632uz <kimotu.phone@gmail.com>
2023-10-22 03:53:38 +0000

    update abc315

--- abc315/d/abc315_d.cpp
+++ abc315/d/abc315_d.cpp
@@ -1,19 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define irep(i, n) for (int i = (n-1); i >= 0; --i)
-#define all(v) v.begin(), v.end()
+constexpr auto seq(int begin, int end) { return ranges::iota_view(begin, end); }
+constexpr auto seq(int end) { return ranges::iota_view(0, end); }
+
+int find_nonzero(vector<int> &v) {
+    int count = 0;
+    int ans = -1;
+    for (int i: seq(26)) {
+        if (v[i] > 0) {
+            ++count;
+            ans = i;
+        }
+    }
+    if (count > 1) {
+        return -1;
+    } else {
+        return ans;
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    int h, w;
-    cin >> h >> w;
-    vector<vector<char>> c(h, vector<char>(w));
-    rep(i, h) rep(j, w) cin >> c[i][j];
-
+    int h, w; cin >> h >> w;
+    vector row(h, vector<int>(26,0)); 
+    vector col(w, vector<int>(26,0)); 
+    vector<bool> aliverow(h, true), alivecol(w, true);
     
+    for (int i: seq(h)) {
+        string s; cin >> s;
+        for (int j: seq(w)) {
+            int c = s[j] - 'a';
+            ++row[i][c];
+            ++col[j][c];
+        }
+    }
+    
+    while(true) {
+        vector<pair<int,int>> remove_row;
+        for (int i: seq(h)) {
+            if (aliverow[i]) {
+                int key = find_nonzero(row[i]);
+                if (key != -1 && row[i][key] > 1) {
+                    remove_row.push_back({i, key});
+                }
+            }
+        }
+
+        vector<pair<int,int>> remove_col;
+        for (int i: seq(w)) {
+            if (alivecol[i]) {
+                int key = find_nonzero(col[i]);
+                if (key != -1 && col[i][key] > 1) {
+                    remove_col.push_back({i, key});
+                }
+            }
+        }
+ 
+        if (remove_row.empty() && remove_col.empty()) break;
+
+        for (auto&& [i, c]: remove_row) {
+            aliverow[i] = false;
+            for (int j: seq(w)) {
+                col[j][c] = max(0, col[j][c]-1);
+            }
+        }
+        for (auto&& [i, c]: remove_col) {
+            alivecol[i] = false;
+            for (int j: seq(h)) {
+                row[j][c] = max(0, row[j][c]-1);
+            }
+        }
+    }
+    
+    cout << ranges::count(aliverow, true) * ranges::count(alivecol, true);
     return 0;
 }
--- abc315/e/abc315_e.cpp
+++ abc315/e/abc315_e.cpp
@@ -1,13 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define irep(i, n) for (int i = (n-1); i >= 0; --i)
-#define all(v) v.begin(), v.end()
+constexpr auto seq(int begin, int end) { return ranges::iota_view(begin, end); }
+constexpr auto seq(int end) { return ranges::iota_view(0, end); }
+
+using Graph = vector<vector<int>>;
+
+vector<bool> seen;
+vector<int> read_list;
+
+void dfs(Graph& g, int v) {
+    seen[v] = true;
+
+    for (auto&& nv : g[v]) {
+        if (seen[nv]) continue;
+        dfs(g, nv);
+        read_list.push_back(nv);
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
+    int n; cin >> n;
+    Graph g(n);
+    for (int i: seq(n)) {
+        int c; cin >> c;
+        for (int j: seq(c)) {
+            int p; cin >> p;
+            g[i].push_back(p-1);
+        }
+    }
+    
+    seen.assign(n, false);
+    dfs(g, 0);
+    
+    for (auto&& e: read_list) {
+        cout << e+1 << ' ';
+    }
     return 0;
 }
