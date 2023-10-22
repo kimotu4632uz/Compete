fd6fbcc952477d91968a085b30b59a410f92b47e
kimotu4632uz <kimotu.phone@gmail.com>
2023-10-22 10:01:25 +0000

    update abc319

--- abc319/c/abc319_c.cpp
+++ abc319/c/abc319_c.cpp
@@ -1,59 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define irep(i, n) for (int i = (n-1); i >= 0; --i)
-#define all(v) v.begin(), v.end()
+constexpr auto seq(int begin, int end) { return ranges::iota_view(begin, end); }
+constexpr auto seq(int end) { return ranges::iota_view(0, end); }
 
-int toe(int i, int j) {
-    return i*3+j;
+
+tuple<int,int,int> find_dual(vector<int> &v, vector<int> &idx) {
+    if (v[idx[0]] == v[idx[1]]) return {idx[0], idx[1], idx[2]};
+    else if (v[idx[0]] == v[idx[2]]) return {idx[0], idx[2], idx[1]};
+    else if (v[idx[2]] == v[idx[1]]) return {idx[2], idx[1], idx[0]};
+    else return {-1,-1,-1};
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    vector<vector<int>> m(3, vector<int>(3));
-    rep(i,3) rep(j,3) cin >> m[i][j];
+    vector<int> m(9);
+    for (int i: seq(9)) {
+        cin >> m[i];
+    }
 
+    vector<vector<int>> idxs = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
     vector<tuple<int,int,int>> pat;
-    for (int j=0;j<3;j++)
-        if (m[0][j] == m[1][j]) pat.push_back({toe(0,j), toe(1,j), toe(2,j)});
-    for (int j=2;j>=0;j--)
-        if (m[2][j] == m[1][j]) pat.push_back({toe(2,j), toe(1,j), toe(0,j)});
-    for (int i=0;i<3;i++)
-        if (m[i][0] == m[i][1]) pat.push_back({toe(i,0), toe(i,1), toe(i,2)});
-    for (int i=2;i>=0;i--)
-        if (m[i][2] == m[i][1]) pat.push_back({toe(i,2), toe(i,1), toe(i,0)});
-    if (m[0][0] == m[1][1]) pat.push_back({toe(0,0), toe(1,1), toe(2,2)});
-    if (m[2][2] == m[1][1]) pat.push_back({toe(2,2), toe(1,1), toe(0,0)});
-    if (m[2][0] == m[1][1]) pat.push_back({toe(2,0), toe(1,1), toe(0,2)});
-    if (m[0][2] == m[1][1]) pat.push_back({toe(0,2), toe(1,1), toe(2,0)});
+    for (auto&& idx: idxs) {
+        auto [a,b,c] = find_dual(m, idx);
+        if (a != -1) {
+            pat.push_back({a,b,c});
+        }
+    }
 
     vector<int> v(9);
-    iota(all(v), 0);
+    iota(v.begin(), v.end(), 0);
 
     ll all=0, gt=0;
     do {
         bool bad = false;
-        for (auto&& p : pat) {
-            auto [a,b,c] = p;
-            int ai,bi,ci;
-            rep(i,9) {
-                if (v[i] == a) ai=i;
-                else if (v[i] == b) bi=i;
-                else if (v[i] == c) ci=i;
+        for (auto&& [a,b,c] : pat) {
+            int ai, bi, ci;
+            for (int i: seq(9)) {
+                if (v[i] == a) ai = i;
+                if (v[i] == b) bi = i;
+                if (v[i] == c) ci = i;
             }
-            if (ai < bi && bi < ci) {
-                bad = true;
-                break;
-            }
+            if (ai < ci && bi < ci) bad = true;
         }
         if (!bad) ++gt;
         ++all;
 
-    } while(next_permutation(all(v)));
+    } while(ranges::next_permutation(v).found);
 
-    cout << (double)gt/all;
+    cout << fixed << setprecision(15) << (double)gt/all;
     return 0;
 }
--- abc319/d/abc319_d.cpp
+++ abc319/d/abc319_d.cpp
@@ -1,13 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
-#define irep(i, n) for (int i = (n-1); i >= 0; --i)
-#define all(v) v.begin(), v.end()
+constexpr auto seq(int begin, int end) { return ranges::iota_view(begin, end); }
+constexpr auto seq(int end) { return ranges::iota_view(0, end); }
+#define all(v) std::ranges::begin((v)), std::ranges::end((v))
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
+    int n, m; cin >> n >> m;
+    vector<ll> ls(n);
+    for (int i: seq(n)) {
+        ll t; cin >> t;
+        ls[i] = t+1;
+    }
+    
+    ll lower = ranges::max(ls);
+    ll upper = reduce(all(ls));
+
+    auto it = ranges::partition_point(views::iota(lower, upper), [&](ll w) {
+        ll colnum = 1;
+        ll sum = 0;
+        for (auto&& e: ls) {
+            if (sum + e > w) {
+                sum = e; ++colnum;
+            } else {
+                sum += e;
+            }
+        }
+        return colnum > m;
+    });
+    
+    cout << *it - 1;
     return 0;
 }
