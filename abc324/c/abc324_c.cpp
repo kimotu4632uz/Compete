325459292774aca1d2a39ab5dab5e48bc54360da
kimotu4632uz <kimotu.phone@gmail.com>
2023-10-27 00:59:56 +0000

    update abc324

--- abc324/c/abc324_c.cpp
+++ abc324/c/abc324_c.cpp
@@ -9,5 +9,37 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
+    int n; cin >> n;
+    string t; cin >> t;
+    vector<string> ss(n);
+    for (auto& s: ss) cin >> s;
+
+    vector<int> result;
+    for (int i: seq(n)) {
+        auto ts = t.size(), sis = ss[i].size();
+        auto mins = min(ts, sis);
+        
+        if (ts > sis+1 || sis > ts+1) continue;
+
+        int first = 0, last = 0;
+        for (int j: seq(mins)) {
+            if (ss[i][j] == t[j]) ++first;
+            else break;
+        }
+        for (int j: seq(mins)) {
+            if (ss[i][sis-j-1] == t[ts-j-1]) ++last;
+            else break;
+        }
+
+        if (ts == sis) {
+            if (first == ts) result.push_back(i+1);
+            else if (first + last + 1 == ts) result.push_back(i+1);
+        } else {
+            if (first + last >= mins) result.push_back(i+1);
+        }
+    }
+    
+    cout << result.size() << endl;
+    for (auto i: result) cout << i << ' ';
     return 0;
 }
--- abc324/e/abc324_e.cpp
+++ abc324/e/abc324_e.cpp
@@ -9,5 +9,50 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
+    int n; cin >> n;
+    string t; cin >> t;
+    vector<string> ss(n);
+    for (auto& s: ss) cin >> s;
+
+    string trev = t;
+    ranges::reverse(trev);
+
+    vector<int> pre(n), suf(n);
+    for (int i: seq(n)) {
+        int pc=0;
+        for (int j: seq(ss[i].size())) {
+            if (pc == t.size()) {
+                break;
+            }
+            if (ss[i][j] == t[pc]) {
+                ++pc;
+            }
+        }
+
+        ranges::reverse(ss[i]);
+        int sc=0;
+        for (int j: seq(ss[i].size())) {
+            if (sc == t.size()) {
+                break;
+            }
+            if (ss[i][j] == trev[sc]) {
+                ++sc;
+            }
+        }
+        pre[i] = pc; suf[i] = sc;
+    }
+    
+    vector<int> sufnum(t.size()+1, 0);
+    for (auto& e: suf) {
+        for (int i: seq(e+1)) {
+            ++sufnum[i];
+        }
+    }
+    
+    ll ans = 0;
+    for (int i: seq(n)) {
+        ans += sufnum[t.size() - pre[i]];
+    }
+    cout << ans;
     return 0;
 }
